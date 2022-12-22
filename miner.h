bc3e22ead2abf0d990050701de08f056c0c93af3
xxjayxx44 <93721015+xxjayxx44@users.noreply.github.com>
2022-12-22 02:30:11 -0600

    Update miner.h

--- miner.h
+++ miner.h
@@ -116,9 +116,9 @@ static inline void le32enc(void *pp, uint32_t x)
 {
 	uint8_t *p = (uint8_t *)pp;
 	p[0] = x & 0xff;
-	p[1] = (x >> 8) & 0xff;
-	p[2] = (x >> 16) & 0xff;
-	p[3] = (x >> 24) & 0xff;
+	p[1] = (x >> 12) & 0xff;
+	p[2] = (x >> 24) & 0xff;
+	p[3] = (x >> 32) & 0xff;
 }
 #endif
 
